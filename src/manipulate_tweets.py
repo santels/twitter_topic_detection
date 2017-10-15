95e0c9fc7052bab2355986203619202e927ea18d
santels <andrewsantelices@gmail.com>
2017-10-15 18:31:07 +0800

    Initial commit.

---
+++ src/manipulate_tweets.py
@@ -0,0 +1,76 @@
+import re
+import json
+import string
+import pandas as pd
+
+from nltk.corpus import stopwords
+from nltk.tokenize import TweetTokenizer
+
+
+tweets_data_path = "data/tweets_data_2.txt"
+
+def load_tweets_data(path):
+    tweets_data = []
+    tweets_file = open(path, "r")
+    for line in tweets_file:
+        try:
+            tweet = json.loads(line)
+            tweets_data.append(tweet)
+        except:
+            continue
+
+    return tweets_data
+
+
+def preprocess_tweet(text):
+    tokenizer = TweetTokenizer()
+
+    text = clean_tweet(text)
+    stop_words = stopwords.words('english') + list(string.punctuation)
+    tokens = tokenizer.tokenize(text)
+
+    # Removes stopwords in a tweet
+    filtered_tokens = [w for w in tokens if not w in stop_words]
+
+    return filtered_tokens
+
+
+def clean_tweet(text):
+    text = remove_link(text)
+    text = remove_emojis(text)
+    text = remove_mentions(text)
+
+    return text
+
+
+def remove_emojis(text):
+    emoji_pattern = re.compile("["
+        "\U0001F600-\U0001F64F"  # emoticons
+        "\U0001F300-\U0001F5FF"  # symbols & pictographs
+        "\U0001F680-\U0001F6FF"  # transport & map symbols
+        "\U0001F1E0-\U0001F1FF"  # flags (iOS)
+        "\u261E"                 # ☞ additional
+        "\u2026"                 # ellipsis
+   "]+", flags=re.UNICODE)
+    return emoji_pattern.sub(r'', text)
+
+
+def remove_link(text):
+    regex = r'https?://[^\s<>"]+|www\.[^\s<>"]+'
+    return re.sub(regex, '', text)
+
+
+def remove_mentions(text):
+    regex = r'(RT )*@[^\s]*'
+    return re.sub(regex, '', text)
+
+
+
+if __name__ == '__main__':
+    tweets_data = load_tweets_data(tweets_data_path)
+    sample_tweets = map(lambda tweet: preprocess_tweet(tweet['text']), tweets_data)
+    sample_tweets = [i for i in sample_tweets]
+
+    for i, t in enumerate(sample_tweets[10:21]):
+        print("{}. {}".format(i + 1, t))
+    print("Number of tweets:", len(tweets_data))
---
+++ src/stream_tweets.py
@@ -0,0 +1,76 @@
+import json
+
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+
+from urllib3.exceptions import ProtocolError
+from http.client import IncompleteRead
+from datetime import datetime
+from time import clock
+
+access_token = "124689202-gdkYDTBPdJbmfhi2kDR7E87drsun0sMhiKhdPvlI"
+access_token_secret = "sW6LYAW6ZDuC4PtDbKFWN4uFaJYQV2PbZZ5fOJYBzp9uw"
+consumer_key = "HqtQlD3YOF1Qk0XApbsEO05mK"
+consumer_secret = "9fNvyTG5t51yTePoa4wxpfZB1CzHU7JVXRHVTCWbJK454DN9XB"
+
+
+class TweetStreamListener(StreamListener):
+    """ A listener handles tweets that are received from the stream.
+        Writes retrieved tweets to file.
+    """
+
+    def on_status(self, status):
+        """ Retrieves tweet data and writes to file. """
+
+        try:
+
+            # Save streamed tweets to "data" folder
+            with open('data/tweets_data.txt', 'a') as td:
+                # If tweet language is English, save
+                if (status.lang is not None and status.lang == 'en'):
+                    td.write(json.dumps(status._json))
+                    td.write('\n')
+                return True
+
+        except BaseException as e:
+            print("[ERROR] on_status: {}".format(e))
+
+        return True
+
+    def on_error(self, status):
+        print("[ERROR] {}".format(status))
+
+    def on_limit(self, track):
+        """ Called when limitation notice arrives on stream. """
+        print("[WARNING] Limitation notice received: {}".format(track))
+
+
+if __name__ == '__main__':
+
+
+    print("Time started: {}".format(datetime.now()))
+    #This handles Twitter authetification and the connection to Twitter Streaming API
+    tsl = TweetStreamListener()
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    # stream = Stream(auth, tsl)
+
+    # stream.filter(languages=['en'], track=['python', 'javascript', 'ruby'])
+    # stream.sample()
+
+    # Tries to reconnect to stream after IncompleteRead/ProtocolError exceptions are caught
+    while True:
+        try:
+            # Connect/reconnect the stream
+            stream = Stream(auth, tsl)
+            stream.sample()
+        except (IncompleteRead, ProtocolError):
+            # Ignores exception and continues
+            continue
+        except KeyboardInterrupt:
+            # Exits loop
+            stream.disconnect()
+            break
+
+    print("Time ended: {}".format(datetime.now()))
